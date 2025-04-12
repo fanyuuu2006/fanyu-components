47a3451683e3543e54e281c789e6e171437a996b
Yorn90104 <bingxiao526@gmail.com>
2025-04-12 23:00:12 +0800

    fix: 冷卻按鈕樣式分配問題

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { OutsideLink } from "./components/OutsideLink";
 import { CoolDownButton } from "./components/CoolDownButton";
-import { useModal } from './components/Modal';
+import { useModal } from "./components/Modal";
+import { DownloadButton } from "./components/DownloadButton";
 
-export { OutsideLink, CoolDownButton, useModal};
+export { OutsideLink, CoolDownButton, useModal, DownloadButton };
