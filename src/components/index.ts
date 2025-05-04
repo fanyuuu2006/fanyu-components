543c68c76aeab312818e33865d59acfd22af894c
Yorn90104 <bingxiao526@gmail.com>
2025-05-04 23:09:36 +0800

    fix: 忘記匯出組件了

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { CoolDownButton } from "./CoolDownButton";
 export { useModal } from "./Modal";
 export { DownloadButton } from "./DownloadButton";
 export { StateStylesComponent } from "./StateStylesComponent";
+export { Collapse } from "./Collapse";
